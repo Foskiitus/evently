a1dc6fd5928f2293ec8638957ebd151cddc00ff6
Foskiitus <foskiitus@gmail.com>
2024-04-28 11:57:26 +0200

    Package downgraded for tutorial purposes

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
-    '/forum(.*)',
-  ]);
-
-
-  export default clerkMiddleware((auth, req) => {
-    // Restrict admin routes to users with specific permissions
-    if (isProtectedRoute(req)) {
-      auth().protect(has => {
-        return (
-          has({ permission: 'org:sys_memberships:manage' }) ||
-          has({ permission: 'org:sys_domains_manage' })
-        )
-      })
-    }
-  });
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+});
 
 export const config = {
-    matcher: [
-      '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-      '/', // Run middleware on index page
-      '/(api|trpc)(.*)'], // Run middleware on API routes
-  };
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
